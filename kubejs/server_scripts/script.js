cfe342e75c70355b4e01ccf962f811f42c95fb42
PreciseModpacker <precise.modpacker@gmail.com>
2024-07-27 13:43:31 +0200

    Remove all Villager Trade recipes from JEI

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1154,3 +1154,15 @@ ServerEvents.recipes(event => {
 // ====================================================================================================================
 // ====================================================================================================================
 });
+
+MoreJSEvents.villagerTrades((event) => {
+    /*
+     * Will remove all vanilla trades. You can also just remove them for specific professions.
+     */
+    event.removeVanillaTrades();
+
+    /*
+     * Will remove all mod trades. You can also just remove them for specific professions.
+     */
+    event.removeModdedTrades();
+});
