16478e16f99487180ecdc3b47264cae63c6101c3
PreciseModpacker <precise.modpacker@gmail.com>
2026-05-19 15:56:14 +0200

    Ban Chain Lightning, Potato Recovery & Capacity enchanted books from the loot

--- kubejs/server_scripts/loot.js
+++ kubejs/server_scripts/loot.js
@@ -3,4 +3,12 @@ LootJS.modifiers((event) => {
         .addLootTypeModifier(LootType.CHEST)
         .replaceLoot("minecraft:golden_apple", "minecraft:iron_apple", true)
         .replaceLoot("minecraft:enchanted_golden_apple", "minecraft:enchanted_iron_apple", true)
-    });    
+    });
+LootJS.modifiers((event) => {
+    // Remove enchanted books with unwanted enchantments from all loot
+    event
+        .addLootTypeModifier(LootType.CHEST, LootType.FISHING, LootType.ENTITY, LootType.BLOCK)
+        .removeLoot(ItemFilter.hasEnchantment("dragonenchants:chain_lightning"))
+        .removeLoot(ItemFilter.hasEnchantment("create:potato_recovery"))
+        .removeLoot(ItemFilter.hasEnchantment("create:capacity"))
+});
